423706d1e3eaa7479fa78cd43cf4925634748057
Goncalo Barroso <goncalo@automationlogic.com>
2019-08-06 17:33:56 +0100

    Remove envi debug

--- envi.sh
+++ envi.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # This script will spawn a child bash shell, inject vars from envis folder or local envi.json and run the command
 
 set -e
-set -x # Uncomment to print which command is running
+#set -x # Uncomment to print which command is running
 
 if [ -d ~/.envis ]; then
   for file in ~/.envis/*.json; do
